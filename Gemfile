0936dacad01fa942446fe5403b3b52deccec035e
Jan Klimo <jan.klimo@gmail.com>
2017-02-02 18:39:38 +0700

    back to RMagick

--- Gemfile
+++ Gemfile
@@ -15,7 +15,6 @@ gem "paperclip", "~> 5.0.0"
 gem 'aws-sdk', '~> 2.3.0'
 gem 'sidekiq'
 gem 'has_secure_token'
-gem "mini_magick"
 
 gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
--- app/workers/order_worker.rb
+++ app/workers/order_worker.rb
@@ -1,34 +1,29 @@
-# require 'RMagick'
+require 'RMagick'
 
 class OrderWorker
   include Sidekiq::Worker
-  # include Magick
+  include Magick
 
   def perform(order_id)
     order = Order.find(order_id)
     album = order.albums.create
 
-    image = MiniMagick::Image.open(Rails.root.join('app/assets/images/image.jpg'))
-    image.combine_options do |b|
-      b.resize("1200x630>")
-      b.blur 3
-      b.flop
-    end
-    # logo = Image.read(Rails.root.join('app/assets/images/hq.png')).first
+    image = Image.read(Rails.root.join('app/assets/images/image.jpg')).first
+    logo = Image.read(Rails.root.join('app/assets/images/hq.png')).first
 
-    # quote = "##{rand(10) + 1} A person who never made a mistake never tried anything new"
-    # caption = Image.read("caption:#{quote.upcase}") do
-    #   self.size = "450x300"
-    #   self.pointsize = 32
-    #   self.font = "#{Rails.root}/app/assets/fonts/catamaran.ttf"
-    #   self.gravity = CenterGravity
-    #   self.background_color = '#0005'
-    #   self.fill = 'white'
-    # end.first
-    #
-    # image.resize_to_fill!(1200, 630)
-    # image.composite!(caption, CenterGravity, OverCompositeOp)
-    # image.composite!(logo.resize_to_fit!(300, 300), SouthEastGravity, 20, 20, OverCompositeOp)
+    quote = "##{rand(10) + 1} A person who never made a mistake never tried anything new"
+    caption = Image.read("caption:#{quote.upcase}") do
+      self.size = "450x300"
+      self.pointsize = 32
+      self.font = "#{Rails.root}/app/assets/fonts/catamaran.ttf"
+      self.gravity = CenterGravity
+      self.background_color = '#0005'
+      self.fill = 'white'
+    end.first
+
+    image.resize_to_fill!(1200, 630)
+    image.composite!(caption, CenterGravity, OverCompositeOp)
+    image.composite!(logo.resize_to_fit!(300, 300), SouthEastGravity, 20, 20, OverCompositeOp)
 
     file = Paperclip::Tempfile.new(["#{order_id}#{rand(36**20).to_s(36)}", ".jpg"])
     image.write(file.path)
@@ -37,10 +32,10 @@ class OrderWorker
     album.results << result
 
     # cleanup
-    # caption.destroy!
-    # image.destroy!
-    # logo.destroy!
-    # GC.start
+    caption.destroy!
+    image.destroy!
+    logo.destroy!
+    GC.start
 
     order.update(status: :completed)
   end
